a84dd2137c2d353b3bb84478eec927979107cb3c
ahmed7761 <sharif7761@gmail.com>
2024-08-13 23:36:59 +0600

    routing and apollo client setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ApolloProvider } from "@apollo/client";
+import client from "./ApolloClient";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ApolloProvider>,
 )
